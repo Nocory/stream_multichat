b9217228becf7791ec387411911d1aa24cb850f0
Conroy <example@example.com>
2023-04-15 17:27:45 +0200

    finalize viewer-count page

--- pages/viewer-count.vue
+++ pages/viewer-count.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="viewer-count">
+  <div class="viewer-count-page">
     <div
       v-if="routeParams.kick"
       data-platform="kick"
-      class="kick"
+      class="viewer-count-number kick"
       v-text="kickViewerCount"
     />
   </div>
@@ -13,23 +13,24 @@
 import { useIntervalFn, useUrlSearchParams } from "@vueuse/core"
 
 const params = useUrlSearchParams("history")
-
 const routeParams = { kick: params.kick as string }
 
 const kickViewerCount = ref(0)
 
-if (routeParams.kick) {
-  useIntervalFn(async () => {
-    try {
-      const apiResponse = await fetch(`https://kick.com/api/v2/channels/${routeParams.kick}/`)
-      if (!apiResponse.ok) {
-        throw new Error("Fetching of Kick channel information failed. Response was not OK")
-      }
-      const parsedApiResponse = await apiResponse.json()
-      kickViewerCount.value = parsedApiResponse?.livestream?.viewer_count ?? 0
-    } catch (error) {
-      console.error(error)
+const fetchKickViewerCount = async () => {
+  try {
+    const response = await fetch(`https://kick.com/api/v2/channels/${routeParams.kick}/`)
+    if (!response.ok) {
+      throw new Error(`Fetching of Kick channel information failed with status ${response.status}: ${response.statusText}`)
     }
-  }, 30000, { immediateCallback: true })
+    const parsedResponse = await response.json()
+    kickViewerCount.value = parsedResponse?.livestream?.viewer_count ?? 0
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+if (routeParams.kick) {
+  useIntervalFn(fetchKickViewerCount, 30000, { immediateCallback: true })
 }
 </script>
