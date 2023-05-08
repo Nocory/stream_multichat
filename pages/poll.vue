9dad820d334459de516b0463bade394d7445efe5
Conroy <Nocory@users.noreply.github.com>
2023-05-08 16:22:12 +0200

    rename symbols and implement yes and no polls

--- pages/poll.vue
+++ pages/poll.vue
@@ -1,22 +1,25 @@
 <template>
-  <div v-if="voteOptions.length !== 0" class="h-screen overflow-hidden">
-    <div class="flex flex-col items-stretch w-full">
+  <div v-if="pollOptions.length !== 0" class="h-screen overflow-hidden">
+    <div class="flex flex-col gap-1 items-stretch w-full">
+      <div class="bg-slate-900/90 text-white flex justify-center font-bold">
+        {{ yesNoTitle }}
+      </div>
       <div
-        v-for="(option, index) in voteOptions"
+        v-for="(option, index) in pollOptions"
         :key="option"
         class="flex flex-col"
       >
-        <div class="flex flex-col flex-1 mb-1 border border-white">
+        <div class="flex flex-col flex-1 border border-white">
           <div class="flex">
             <div class="font-bold w-10 flex justify-center items-center bg-slate-50 leading-none">
-              {{ index + 1 }}
+              {{ yesNoTitle ? pollOptions[index] : index + 1 }}
             </div>
             <div
               class="flex items-center bg-slate-500/90 h-8 relative flex-1"
               :class="{
-                '!h-6': voteOptions.length > 4 && voteOptions.length <= 6,
-                '!h-5': voteOptions.length > 6 && voteOptions.length <= 8,
-                '!h-4': voteOptions.length > 8,
+                '!h-6': pollOptions.length > 4 && pollOptions.length <= 6,
+                '!h-5': pollOptions.length > 6 && pollOptions.length <= 8,
+                '!h-4': pollOptions.length > 8,
               }"
             >
               <div
@@ -26,7 +29,7 @@
               <div
                 class="z-50 flex items-center gap-2 mx-2 font-bold leading-none text-white flex-1"
               >
-                <div v-if="showOptionNames" class="flex-1">
+                <div v-if="showOptionNames && !yesNoTitle" class="flex-1">
                   {{ option }}
                 </div>
                 <div v-if="talliedVotes[index + 1]" class="relative z-50">
@@ -55,8 +58,9 @@ import { ChatMessage } from "~/types/common"
 
 const VOTE_GRACE_PERIOD_MS = 5000
 
-const voteOptions = ref<string[]>([])
+const pollOptions = ref<string[]>([])
 const showOptionNames = ref(false)
+const yesNoTitle = ref("")
 const votes = ref<Record<string, {
   choice: number,
   timestamp: number,
@@ -72,24 +76,29 @@ const talliedVotes = computed(() => {
 
 const totalVotes = computed(() => Object.keys(votes.value).length)
 
-const startVote = (voteNames: string[]) => {
-  voteOptions.value = voteNames
+const startPoll = (newOptions: string[]) => {
+  if (newOptions.length === 1) {
+    yesNoTitle.value = newOptions[0]
+    newOptions = ["ja", "nein"]
+  }
+  pollOptions.value = newOptions
   // filter previously cast votes by grace period and check if choice is in valid range
   const now = Date.now()
   votes.value = Object.fromEntries(Object.entries(votes.value).filter(([key, value]) => {
-    return (now - value.timestamp < VOTE_GRACE_PERIOD_MS) && (value.choice >= 1 && value.choice <= voteNames.length)
+    return (now - value.timestamp < VOTE_GRACE_PERIOD_MS) && (value.choice >= 1 && value.choice <= newOptions.length)
   }))
 }
 
-const stopVote = () => {
-  voteOptions.value = []
+const stopPoll = () => {
+  pollOptions.value = []
   votes.value = {}
+  yesNoTitle.value = ""
 }
 
 const handleCommand = (command: string) => {
   // handle poll stop
   if (command === "!poll stop" || command === "!poll end" || command === "!poll ende") {
-    stopVote()
+    stopPoll()
     return
   }
 
@@ -105,12 +114,12 @@ const handleCommand = (command: string) => {
     return
   }
 
-  if (voteOptions.value.length !== 0) return
+  if (pollOptions.value.length !== 0) return
 
   // handle poll start with default options
   if (command === "!poll") {
     showOptionNames.value = false
-    startVote(["1", "2"])
+    startPoll(["1", "2"])
     return
   }
 
@@ -121,7 +130,7 @@ const handleCommand = (command: string) => {
     if (parsedNumber >= 0 && parsedNumber <= 100) {
       const stringOptions = Array.from({ length: parsedNumber }, (_, i) => `${i + 1}`)
       showOptionNames.value = false
-      startVote(stringOptions)
+      startPoll(stringOptions)
     }
     return
   }
@@ -134,21 +143,28 @@ const handleCommand = (command: string) => {
     const items = itemsString.split(/\s*,\s*/)
     if (items.length > 0 && items.length <= 100) {
       showOptionNames.value = true
-      startVote(items)
+      startPoll(items)
     }
   }
 }
 
 const checkForChoice = (identifier: string, message: string) => {
-  const numberChoice = message.match(/^(\d+)[ ,]?/i)?.[0]
-
-  if (numberChoice) {
-    const numericChoice = parseInt(numberChoice)
+  if (yesNoTitle.value) {
+    const yesNoChoice = message.match(/^(ja|nein)[ ,]?/i)?.[1].toLowerCase()
+    if (!yesNoChoice) return
+    votes.value[identifier] = {
+      choice: yesNoChoice === "ja" ? 1 : 2,
+      timestamp: Date.now()
+    }
+  } else {
+    const choice = message.match(/^(\d+)[ ,]?/i)?.[1]
+    if (!choice) return
+    const parsedChoice = parseInt(choice)
     if (
-      voteOptions.value.length === 0 ||
-      (numericChoice >= 1 && numericChoice <= voteOptions.value.length)) {
+      pollOptions.value.length === 0 ||
+      (parsedChoice >= 1 && parsedChoice <= pollOptions.value.length)) {
       votes.value[identifier] = {
-        choice: numericChoice,
+        choice: parsedChoice,
         timestamp: Date.now()
       }
     }
@@ -179,7 +195,7 @@ const simulateVotes = async () => {
       messageParts: [
         {
           type: "text",
-          value: Math.floor(Math.random() * voteOptions.value.length + 1).toString(),
+          value: Math.floor(Math.random() * pollOptions.value.length + 1).toString(),
         }
       ],
       isDeleted: false,
