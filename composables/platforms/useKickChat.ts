92dcded90cffbbd0469bd4f0f236fac4267d797f
Nocory <Nocory@users.noreply.github.com>
2024-07-24 21:00:51 +0200

    update Kick websocket URL

--- composables/platforms/useKickChat.ts
+++ composables/platforms/useKickChat.ts
@@ -92,7 +92,7 @@ export default function(
     }
   }
 
-  const websocket = useWebSocket("wss://ws-us2.pusher.com/app/eb1d5f283081a78b932c?protocol=7&client=js&version=7.6.0&flash=false", {
+  const websocket = useWebSocket("wss://ws-us2.pusher.com/app/32cbd69e4b950bf97679?protocol=7&client=js&version=7.6.0&flash=false", {
     onConnected: socket => {
       try {
         handleConnected(socket)
